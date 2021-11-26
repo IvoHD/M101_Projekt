e05d9a7bedb2829dd47bc3e633efb630f8f3609f
Administrator adm-virtedu <ivo_veigagrenhas1@sluz.ch>
2021-11-26 11:22:15 +0100

    prototyp

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-Lorem Ipsum
+window.onscroll = function() 
+{
+    funcScroll()
+};
+
+
+function funcScroll()
+{
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+    {
+        document.getElementById("gototopbtn").style.display = "inline-block";
+    }   
+    else
+    {
+    document.getElementById("gototopbtn").style.display = "none";
+    }
+}
+
+
+function gototop()
+{
+    window.scrollTo(0, 0);
+}
