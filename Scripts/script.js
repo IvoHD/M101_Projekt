40d351b90ddb184df4d193d1d9733712e5f92cdb
Administrator adm-virtedu <ivo_veigagrenhas1@sluz.ch>
2021-12-03 11:16:36 +0100

    restructure, folders implementet. gototopbtn now working. Galery protoype Flex prototype

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,11 +8,11 @@ function funcScroll()
 {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
     {
-        document.getElementById("gototopbtn").style.display = "inline-block";
+        document.getElementById("gototopbtn").classList.add("show");
     }   
     else
     {
-    document.getElementById("gototopbtn").style.display = "none";
+        document.getElementById("gototopbtn").classList.remove("show");
     }
 }
 
