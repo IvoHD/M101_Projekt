a0e1174f7571ec2303f61baeb5e9feb497c408b0
IvoHD <ivo10ivo@hotmail.com>
2022-01-21 00:23:24 +0100

    more progress

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,24 +1,19 @@
-window.onscroll = function() 
-{
+window.onscroll = function() {
     funcScroll()
 };
 
 
-function funcScroll()
-{
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
-    {
-        document.getElementById("gototopbtn").classList.add("show");
-    }   
-    else
-    {
-        document.getElementById("gototopbtn").classList.remove("show");
-    }
+function funcScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) document.getElementById("gototopbtn").classList.add("show");
+    else document.getElementById("gototopbtn").classList.remove("show");
 }
 
 
-function gototop()
-{
+function gototop() {
     window.scrollTo(0, 0);
+}
+
+function formmessage() {
+    alert("Vielen Dank für die Nachricht, wir werden Sie sobald wie möglich Kontaktieren.");
 }
 
